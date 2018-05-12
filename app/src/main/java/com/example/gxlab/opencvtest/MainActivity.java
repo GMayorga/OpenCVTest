970007c8e6beb87080588398a28e8cd6664db878
Xuan Do <doxuancntp@gmail.com>
2018-05-12 15:37:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gxlab/opencvtest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.gxlab.opencvtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MainActivity";
+
+    static {
+        if (OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV successfully loaded");
+        } else {
+            Log.d(TAG, "OpenCV not Loaded");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
